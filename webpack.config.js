5d3c343f89ac178dcc7153a8447e1953aa5cda2f
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2017-12-23 13:24:53 +0900

    babel-preset-es2015を@babel/preset-envに変更

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['es2015', 'react', 'stage-0']
+          presets: ['react', 'stage-0']
         }
       }
     ]
